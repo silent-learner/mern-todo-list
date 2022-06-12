be28de51983f08ffa671ef14f34ebb9f27145cf3
Mohammad Kashif Khan <kk11122002@gmail.com>
2022-06-12 12:13:23 +0530

    Deploying

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ app.use("/get", require("./routes/getitems.js"))
 app.use("/post", require("./routes/postitems.js"))
 app.use("/delete", require("./routes/deleteitems.js"))
 app.use("/", require("./routes/createUser.js"))
-app.get("/*", (req, res) => {
-    res.status(404).json({ success: false, "message": "Page Doesn't Exist" })
-})
+app.use(express.static(path.join(__dirname, "client", "build")))
+// app.get("/*", (req, res) => {
+//     res.status(404).json({ success: false, "message": "Page Doesn't Exist" })
+// })
 
 // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 // ...
 
@@ -30,12 +31,13 @@ mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MongoDB connected")
         // Right before your app.listen(), add this:
-        app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-        });
 
-        app.listen(PORT, () => {
-            console.log(`Server running at http://localhost:${PORT}`);
-        })
     })
     .catch(err => console.log(err.message))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
